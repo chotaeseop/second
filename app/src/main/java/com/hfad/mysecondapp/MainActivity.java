a98b95b21fea0b4f2c33ee357eb5af17f95f2927
chotaeseop <jo123123123@knou.ac.kr>
2019-02-10 18:46:31 +0900

    fix

--- app/src/main/java/com/hfad/mysecondapp/MainActivity.java
+++ app/src/main/java/com/hfad/mysecondapp/MainActivity.java
@@ -18,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void test(){
-        
+        System.out.print("");
     }
 }
